2636284e483e13109d88ea091f2c4039e35d11de
Afghan12a <159397881+Afghan12a@users.noreply.github.com>
2024-05-28 22:17:37 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    // Define an interface IQuittable
+    public interface IQuittable
+    {
+        // Define a void method Quit in the interface
+        void Quit();
+    }
+
+    // Define the Employee class that implements the IQuittable interface
+    class Employee : IQuittable
+    {
+        // Implementation of the Quit method from IQuittable
+        public void Quit()
+        {
+            // Print a message when the Quit method is called
+            Console.WriteLine("Employee has quit the job!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of type IQuittable using polymorphism
+            IQuittable quittableEmployee = new Employee();
+
+            // Call the Quit method on the object
+            quittableEmployee.Quit();
+        }
+    }
+}
